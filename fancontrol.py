7550ebf7d24fea2abeea36f7db9171319d2ac967
Alain FERRARO <11302604+afer92@users.noreply.github.com>
2021-03-20 23:34:05 +0100

    Greater hysteresis and low voltage.

--- fancontrol.py
+++ fancontrol.py
@@ -6,10 +6,12 @@ import time
 from gpiozero import PWMLED
 
 
-TEMP_MIN = 40 # Temperature (or lower) where fan voltage is 0.0
-TEMP_MAX = 49 # Temperature (or higher) where van voltage is 1.0
-SLEEP_INTERVAL = 2  # (seconds) How often we check the core temperature.
-GPIO_PIN = 18  # Which GPIO pin you're using to control the fan.
+TEMP_MIN = 55 # Temperature (or lower) where fan voltage is 0.0
+TEMP_MAX = 65 # Temperature (or higher) where van voltage is 1.0
+SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
+GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
+HIGH_PERCENT = 0.6
+LOW_PERCENT = 0.4
 
 
 def get_temp():
@@ -31,12 +33,16 @@ def get_temp():
         raise RuntimeError('Could not parse temperature output.')
 
 def get_voltage():
-    r = TEMP_MAX - TEMP_MIN
+    r = TEMP_MAX - TEMP_MIN # delta consigne
+    dv = HIGH_PERCENT - LOW_PERCENT
+    step = dv / r
     v = get_temp() - TEMP_MIN
-    v = v / r
-    if v < 0.0001: return 0.0
-    elif v > 0.9998: return 1.0
-    else: return v
+    if v <= 0:
+        return 0
+    v = LOW_PERCENT + (v * step)
+    if v > 1:
+        return 1
+    return v
 
 if __name__ == '__main__':
     # Validate
@@ -45,7 +51,19 @@ if __name__ == '__main__':
 
     fan = PWMLED(GPIO_PIN)
     fan.on()
+    fan_on = False
+    fan.value = 0
+    temp_seuil = (TEMP_MAX + TEMP_MIN) / 2
 
     while True:
-        fan.value = get_voltage()
+        temp = get_temp()
+        if not fan_on and temp > temp_seuil:
+            fan_on = True
+        if temp < TEMP_MIN:
+            fan_on = False
+            fan.value = 0
+        if fan_on: 
+            voltage = get_voltage()
+            fan.value = voltage
+        # print(u'temp: {} voltage: {}'.format(temp, fan.value))
         time.sleep(SLEEP_INTERVAL)
