d77653961061accd42ed88e88d27d9cc483dc81e
Alain Ferraro <11302604+afer92@users.noreply.github.com>
2021-03-29 17:55:44 +0200

    Add test functionality

--- fancontrol.py
+++ fancontrol.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-
+import argparse
 import subprocess
 import time
 
 from gpiozero import PWMLED
 
 
-TEMP_MIN = 55 # Temperature (or lower) where fan voltage is 0.0
-TEMP_MAX = 65 # Temperature (or higher) where van voltage is 1.0
+TEMP_MIN = 40  # Temperature (or lower) where fan voltage is 0.0
+TEMP_MAX = 50  # Temperature (or higher) where van voltage is 1.0
 SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
 GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
 HIGH_PERCENT = 0.6
 LOW_PERCENT = 0.4
+TEST = False
 
 
 def get_temp():
@@ -32,8 +33,9 @@ def get_temp():
     except (IndexError, ValueError):
         raise RuntimeError('Could not parse temperature output.')
 
+
 def get_voltage():
-    r = TEMP_MAX - TEMP_MIN # delta consigne
+    r = TEMP_MAX - TEMP_MIN  # delta consigne
     dv = HIGH_PERCENT - LOW_PERCENT
     step = dv / r
     v = get_temp() - TEMP_MIN
@@ -44,10 +46,71 @@ def get_voltage():
         return 1
     return v
 
-if __name__ == '__main__':
+
+def print_infos():
+    print(u'GPIO_PIN : \t{}'.format(GPIO_PIN))
+    print(u'SLEEP : \t{}s'.format(SLEEP_INTERVAL))
+    print(u'TEMP_MIN : \t{}°C'.format(TEMP_MIN))
+    print(u'TEMP_MAX : \t{}°C'.format(TEMP_MAX))
+    print(u'LOW_PERCENT : \t{}% ({}V)'.format(LOW_PERCENT, LOW_PERCENT*5))
+    print(u'HIGH_PERCENT : \t{}% ({}V)'.format(HIGH_PERCENT, HIGH_PERCENT*5))
+    print(u'CPU_TEMP : \t{}°C'.format(get_temp()))
+    percent = get_voltage()
+    print(u'VOLTAGE : \t{:.2f}% ({:.2f}V)'.format(percent, percent*5))
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infos",
+                        help="print parameters, tempcpu and voltage",
+                        action="store_true")
+    parser.add_argument("-v", "--verbose",
+                        help="verbose",
+                        action="store_true")
+    parser.add_argument("-m", "--temp_min",
+                        help="TEMP_MIN", type=int)
+    parser.add_argument("-M", "--temp_max",
+                        help="TEMP_MAX", type=int)
+    parser.add_argument("-p", "--percent_low",
+                        help="LOW_PERCENT", type=float)
+    parser.add_argument("-P", "--percent_high",
+                        help="HIGH_PERCENT", type=float)
+    parser.add_argument("-t", "--test",
+                        help="test with parameters", type=int)
+    args = parser.parse_args()
+
+    if args.infos:
+        print_infos()
+        exit(0)
+
+    global TEMP_MIN
+    global TEMP_MAX
+    global LOW_PERCENT
+    global HIGH_PERCENT
+
+    if args.temp_min:
+        TEMP_MIN = args.temp_min
+    if args.temp_max:
+        TEMP_MAX = args.temp_max
+    if args.percent_low:
+        LOW_PERCENT = args.percent_low
+    if args.percent_high:
+        HIGH_PERCENT = args.percent_high
+
     # Validate
     if TEMP_MIN >= TEMP_MAX:
         raise RuntimeError('TEMP_MIN must be less than TEMP_MAX')
+
+    if args.verbose:
+        print_infos()
+
+    count = 1
+    if args.test:
+        count = args.test
+        TEST = True
+
+    # exit(0)
 
     fan = PWMLED(GPIO_PIN)
     fan.on()
@@ -55,15 +118,22 @@ if __name__ == '__main__':
     fan.value = 0
     temp_seuil = (TEMP_MAX + TEMP_MIN) / 2
 
-    while True:
+    while count > 0:
+        if TEST:
+            count += -1
         temp = get_temp()
         if not fan_on and temp > temp_seuil:
             fan_on = True
         if temp < TEMP_MIN:
             fan_on = False
             fan.value = 0
-        if fan_on: 
+        if fan_on:
             voltage = get_voltage()
             fan.value = voltage
-        # print(u'temp: {} voltage: {}'.format(temp, fan.value))
+        if TEST:
+            print(u'temp: {} voltage: {}'.format(temp, fan.value))
         time.sleep(SLEEP_INTERVAL)
+
+
+if __name__ == '__main__':
+    main()
